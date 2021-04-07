a0838feb192e2d26717f61d608534fd889e311df
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-07 13:15:49 -0700

    index.js route

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 // Require route files/folders here
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
 // router.use routes here
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
