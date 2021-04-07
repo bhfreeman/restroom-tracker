e22dad53bdba4318b5b322eee88fda319d3d37cd
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-04-07 13:42:52 -0700

    added api folder and search route folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 // Require route files/folders here
+const apiRoutes = require('./api');
 
 // router.use routes here
+router.use('/api', apiRoutes);
+
 
 module.exports = router;
