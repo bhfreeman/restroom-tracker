f665fac8ca698cf86f65bf86141cea15ffe0df12
bhfreeman <brett.h.freeman@gmail.com>
2021-04-07 11:08:15 -0700

    server file up

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const hbs = exphbs.create({helpers});
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({alter: true}).then(()=> {
+    app.listen(PORT, () => console.log('app running on port ' + PORT))
+})
