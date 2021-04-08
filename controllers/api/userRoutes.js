b129c9c467a1d4303a012d6a3b661d8fab1d36c1
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-08 13:30:37 -0700

    user and home routes update

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,49 +4,97 @@ const sequelizeStore = require("connect-session-sequelize")(session.Store);
 const { User, Review, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-
-///get myreviews
-router.get("/", async (req, res) => {
-  try {
-    const reviewData = await User.findAll({
-        
-    });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
+//user dashboard?
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
     }
+  });
 
-    const validPassword = await userData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
+//get myreviews
+router.get('/reviews', (req, res) => {
+    try{
+      const userData = await User.findByPk({
+        where: {
+          id: req.params.id
+        },
+        include: 
+          {
+            model: Review,
+            attributes: ['id', 'title', 'post_text', 'created_at']
+          },
+
+          res.status(200).json(userData),
+      });
+    }catch(err){
+        res.status(500).json(err);
     }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.json({ user: userData, message: "You are now logged in!" });
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+ 
 });
 
-router.post("/logout", (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+//get mycomments?
+router.get('/comments', (req, res) => {
+    try{
+      const userData = await Comment.findOne({
+        where: {
+          id: req.params.id
+        },
+        include:
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "review_id", "timestamps"],
+                include: {
+                    model: Review,
+                    attributes: ['title']
+                }
+            }
+
+            res.status(200).json(userData);
+    })
+    }catch(err){
+        res.status(500).json(err);
+    }
+ 
 });
+
+// Logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 
 module.exports = router;
+
+//get profile info
+// router.get('/:id', (req, res) => {
+//     try{
+//       const userData = await User.findOne({
+//         where: {
+//           id: req.params.id
+//         },
+//         include: [
+//           {
+//             model: Account
+//           }
+//         ],
+//     })
+//     }catch(err){
+//         res.status(500).json(err);
+//     }
+ 
+// });
