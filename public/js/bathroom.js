b2e0685ddc1cfad2cdc4f13489e4054a8dac0852
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-09 14:19:32 -0700

    bathroom form created

--- public/js/bathroom.js
+++ public/js/bathroom.js
@@ -1,3 +1,27 @@
 async function bathroomFormHandler(event) {
     event.preventDefault();
+
+    const business_name = document.querySelector("").value;
+    const street_address = document.querySelector("").value;
+    const
+
+    //create new bathroom
+    const response = await fetch(`/api/bathroom`, {
+        method: "POST",
+        body: JSON.stringify({
+            user_name,
+            comment_text
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace("/bathroom");
+    } else {
+        alert(response.statusText);
+
 }
+
+document.querySelector("").addEventListener("submit", bathroomFormHandler);
