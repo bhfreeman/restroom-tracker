e745de0f0429eb12c05c4084def7cca53bb8be3a
bhfreeman <brett.h.freeman@gmail.com>
2021-04-09 10:35:41 -0700

    updated search.js

--- public/js/search.js
+++ public/js/search.js
@@ -5,17 +5,18 @@ const searchFormHandler = async (event) => {
     const city = document.querySelector('#city-text').value.trim();
     const state = document.querySelector('#state-code').value.trim();
     const regex = new RegExp('^[A-Z]{2}$');
+    console.log(state)
 
     if (city && state) {
-        if(regex.test(state)){
-          const url = `/bathroom/${city}/${state}`;
+        if(regex.test(state.toUpperCase())){
+          const url = `/bathroom/${city}/${state.toUpperCase()}`;
             const response = await fetch(url, {
               method: 'GET',
             //   body: JSON.stringify({ city, state }),
               headers: { 'Content-Type': 'application/json' },
             });
             if (response.ok) {
-              window.location.replace(`/bathroom/${city}/${state}`)
+              window.location.replace(url)
             } else {
               alert(response.statusText);
             }
