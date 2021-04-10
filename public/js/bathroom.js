e6a4f020f9d0827bbd63ef65c15283a5cdefed9d
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-09 22:28:40 -0700

    create bathroom updates

--- public/js/bathroom.js
+++ public/js/bathroom.js
@@ -1,16 +1,29 @@
 async function bathroomFormHandler(event) {
     event.preventDefault();
 
-    const business_name = document.querySelector("").value;
-    const street_address = document.querySelector("").value;
-    const
+    const business_name = document.querySelector("business_name").value;
+    const street_address = document.querySelector("street_address").value;
+    const city = document.querySelection("city").value;
+    const state = document.querySelector("state").value;
+    const zipcode = document.querySelector("zipcode").value;
+    const location_type = document.querySelector("location_type").value;
+    const number_stalls = document.querySelector("number_stalls").value;
+    const ada_compliant = document.querySelector("ada_compliant").value;
+    const overall_rating = document.querySelector("overall_rating").value;
 
     //create new bathroom
     const response = await fetch(`/api/bathroom`, {
         method: "POST",
         body: JSON.stringify({
-            user_name,
-            comment_text
+            business_name,
+            street_address,
+            city,
+            state,
+            zipcode,
+            location_type,
+            number_stalls,
+            ada_compliant,
+            overall_rating
         }),
         headers: {
             "Content-Type": "application/json"
@@ -20,8 +33,8 @@ async function bathroomFormHandler(event) {
     if (response.ok) {
         document.location.replace("/bathroom");
     } else {
-        alert(response.statusText);
-
+        alert(response.statusText)
+    }
 }
 
-document.querySelector("").addEventListener("submit", bathroomFormHandler);
+document.querySelector("column login-form").addEventListener("submit", bathroomFormHandler);
