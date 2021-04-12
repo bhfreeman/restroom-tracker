e5bb115e3535872ff998281bbcf1dc5dc3226e65
bhfreeman <brett.h.freeman@gmail.com>
2021-04-12 11:13:38 -0700

    added date-fns

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,9 @@
+const date_fns = require('date-fns')
+
 module.exports = {
     format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
+    //   // Format date as MM/DD/YYYY
+    //   return date.toLocaleDateString();
+    date_fns.format(new Date(date), 'MM/dd/yyyy')
     }
-
   };
